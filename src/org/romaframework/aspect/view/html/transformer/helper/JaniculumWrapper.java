7e6fe7640ce2ad109195b4e698439dc2ccb74b5c
Tglman <tglman@tglman.com>
2012-07-31 16:07:02 +0200

    fix wrong label render

--- src/org/romaframework/aspect/view/html/transformer/helper/JaniculumWrapper.java
+++ src/org/romaframework/aspect/view/html/transformer/helper/JaniculumWrapper.java
@@ -673,7 +673,7 @@ public class JaniculumWrapper {
 	}
 
 	public static String getInAreaLabel(HtmlViewRenderable component) {
-		if (!(component instanceof HtmlViewContentComponent) && component instanceof HtmlViewInvisibleContentComponent)
+		if (!(component instanceof HtmlViewContentComponent) || component instanceof HtmlViewInvisibleContentComponent)
 			return null;
 		String label = i18NLabel(component);
 		if ("".equals(label))
